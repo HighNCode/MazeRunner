2662e7f1f9d8c91d6f49a012f1ac8cd2701076d0
HighNCode <waleedbinosama@gmail.com>
2024-04-17 10:04:06 +0500

    Benchmarking and Testing

--- a1-solution-main/src/main/java/ca/master/se2aa4/mazerunner/Main.java
+++ a1-solution-main/src/main/java/ca/master/se2aa4/mazerunner/Main.java
@@ -4,6 +4,7 @@ import org.apache.commons.cli.*;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.util.concurrent.TimeUnit;
 
 public class Main {
 
@@ -12,15 +13,22 @@ public class Main {
     public static void main(String[] args) {
         logger.info("** Starting Maze Runner");
         CommandLineParser parser = new DefaultParser();
+        long baselineTime = 0;
         
         CommandLine cmd = null;
         try {
             cmd = parser.parse(getParserOptions(), args);
             String filePath = cmd.getOptionValue('i');
-            MazeReader mazeReader = new MazeReader(); // Create an instance of MazeReader
-            MazeData mazeData = mazeReader.readMazeFromFile(filePath); // Call readMazeFromFile on the instance
+            MazeReader mazeReader = new MazeReader();
+            MazeData mazeData = mazeReader.readMazeFromFile(filePath);
             Maze maze = Maze.createMaze(mazeData.getMazeStructure(), mazeData.getStart(), mazeData.getEnd());
 
+            if (cmd.hasOption("baseline")) {
+                String baselineMethod = cmd.getOptionValue("baseline");
+                baselineTime = measureTime(() -> solveMaze(baselineMethod, maze));
+                System.out.println("Baseline time: " + baselineTime + " ms");
+            }
+            
             if (cmd.hasOption("p")) {
                 logger.info("Validating path");
                 Path path = new Path(cmd.getOptionValue("p"));
@@ -29,10 +37,20 @@ public class Main {
                 } else {
                     System.out.println("incorrect path");
                 }
-            } else {
+            }
+
+            else {
                 String method = cmd.getOptionValue("method");
                 Path path = solveMaze(method, maze);
                 System.out.println(path.getFactorizedForm());
+                
+                long methodTime = measureTime(() -> solveMaze(method, maze));
+                System.out.println("Method time: " + methodTime + " ms");
+
+                if (cmd.hasOption("baseline")) {
+                    double improvement = (double) methodTime / baselineTime;
+                    System.out.printf("Improvement on the path as a speedup: %.2f%n", improvement);
+                }
             }
         } catch (Exception e) {
             System.err.println("MazeSolver failed.  Reason: " + e.getMessage());
@@ -54,6 +72,13 @@ public class Main {
             return new Path();
         }
     }
+    
+    private static long measureTime(Runnable task) {
+        long startTime = System.nanoTime();
+        task.run();
+        long endTime = System.nanoTime();
+        return TimeUnit.MILLISECONDS.convert(endTime - startTime, TimeUnit.NANOSECONDS);
+    }
 
     private static Options getParserOptions() {
         Options options = new Options();
@@ -64,6 +89,8 @@ public class Main {
 
         options.addOption(new Option("p", true, "Path to be verified in maze"));
         options.addOption(new Option("method", true, "Specify which path computation algorithm will be used"));
+        
+        options.addOption(new Option("baseline", true, "Comparison baseline method"));
 
         return options;
     }
